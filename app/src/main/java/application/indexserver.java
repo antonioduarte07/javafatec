94e7b02c2e5b65c39a301595a287e4f6fb346242
antonioduarte07 <131799867+antonioduarte07@users.noreply.github.com>
2024-03-08 19:34:27 +0000

    Aula Servet 08/03

---
+++ app/src/main/java/application/indexserver.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "indexserver", urlPatterns = "/")
+public class indexserver extends HttpServlet {
+    public void doGet(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        String n = request.getParameter("nome");
+        request.setAttribute("nome", n);
+        //response.getWriter().print("Hello " + n);
+        request.getRequestDispatcher(
+            "/resposta.sjp").forward(request, response);
+    }
+}
